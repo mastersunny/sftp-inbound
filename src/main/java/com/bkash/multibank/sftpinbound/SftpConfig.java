5b5ed93022fa5e1800195576d1b29c3e745b5da2
sunny <multibank04@bkash.com>
2020-04-25 03:22:54 +0600

    first commit

---
+++ src/main/java/com/bkash/multibank/sftpinbound/SftpConfig.java
@@ -0,0 +1,77 @@
+package com.bkash.multibank.sftpinbound;
+
+import java.io.File;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.InboundChannelAdapter;
+import org.springframework.integration.annotation.Poller;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.core.MessageSource;
+import org.springframework.integration.file.filters.AcceptOnceFileListFilter;
+import org.springframework.integration.file.remote.session.CachingSessionFactory;
+import org.springframework.integration.file.remote.session.SessionFactory;
+import org.springframework.integration.sftp.filters.SftpSimplePatternFileListFilter;
+import org.springframework.integration.sftp.inbound.SftpInboundFileSynchronizer;
+import org.springframework.integration.sftp.inbound.SftpInboundFileSynchronizingMessageSource;
+import org.springframework.integration.sftp.session.DefaultSftpSessionFactory;
+import org.springframework.messaging.MessageHandler;
+
+import com.jcraft.jsch.ChannelSftp.LsEntry;
+
+@Configuration
+public class SftpConfig {
+
+    @Value("${sftp.host}")
+    private String sftpHost;
+
+    @Value("${sftp.port:22}")
+    private int sftpPort;
+
+    @Value("${sftp.user}")
+    private String sftpUser;
+
+    @Value("${sftp.user}")
+    private String sftpPassword;
+
+    @Value("${sftp.remote.directory.download.filter:*.*}")
+    private String sftpRemoteDirectoryDownloadFilter;
+
+    @Bean
+    public SessionFactory<LsEntry> sftpSessionFactory() {
+        DefaultSftpSessionFactory factory = new DefaultSftpSessionFactory(true);
+        factory.setHost(sftpHost);
+        factory.setPort(sftpPort);
+        factory.setUser(sftpUser);
+        factory.setPassword(sftpPassword);
+        factory.setAllowUnknownKeys(true);
+        return new CachingSessionFactory<>(factory);
+    }
+
+    @Bean
+    public SftpInboundFileSynchronizer sftpInboundFileSynchronizer() {
+        SftpInboundFileSynchronizer fileSynchronizer = new SftpInboundFileSynchronizer(sftpSessionFactory());
+        fileSynchronizer.setDeleteRemoteFiles(false);
+        fileSynchronizer.setRemoteDirectory("replace with remote directory");
+        fileSynchronizer.setFilter(new SftpSimplePatternFileListFilter("*.*"));
+        return fileSynchronizer;
+    }
+
+    @Bean
+    @InboundChannelAdapter(channel = "fromSftpChannel", poller = @Poller(cron = "0/5 * * * * *"))
+    public MessageSource<File> sftpMessageSource() {
+        SftpInboundFileSynchronizingMessageSource source = new SftpInboundFileSynchronizingMessageSource(
+                sftpInboundFileSynchronizer());
+        source.setLocalDirectory(new File("replace with your local directory"));
+        source.setAutoCreateLocalDirectory(true);
+        source.setLocalFilter(new AcceptOnceFileListFilter<File>());
+        return source;
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "fromSftpChannel")
+    public MessageHandler resultFileHandler() {
+        return message -> System.err.println(message.getPayload());
+    }
+
+}
---
+++ src/main/java/com/bkash/multibank/sftpinbound/SftpInboundApplication.java
@@ -0,0 +1,13 @@
+package com.bkash.multibank.sftpinbound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SftpInboundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SftpInboundApplication.class, args);
+	}
+
+}
